3ed77038857cbea092fee73600b868690b78837e
Nishant Mazumder <nishant.mazumder@gmail.com>
2025-03-13 16:13:23 +0530

    Added main folder

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "PES2UG22CS367_Nishant_Mazumder\n"
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
